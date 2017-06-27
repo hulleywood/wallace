e92497fd99833313d725066e2204e64825a579cc
James Hulley <james.hulley@gmail.com>
2017-06-26 23:18:28 -0700

    move gpios into constants

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,18 @@
 #include <iostream>
 #include "driver.h"
 
+using namespace std;
+
 int main() {
 
-  Driver driver("27", "22", "24", "25", "17", "4");
+  const string gpio_in_1_num = "27";
+  const string gpio_in_2_num = "22";
+  const string gpio_in_3_num = "24";
+  const string gpio_in_4_num = "25";
+  const string gpio_pwm_1_num = "17";
+  const string gpio_pwm_2_num = "4";
+
+  Driver driver(gpio_in_1_num, gpio_in_2_num, gpio_in_3_num, gpio_in_4_num, gpio_pwm_1_num, gpio_pwm_2_num);
 
   return 0;
 
